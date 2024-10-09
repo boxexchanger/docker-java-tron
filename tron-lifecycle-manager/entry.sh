207b83b1beae0216358c179d5fa05ab3e916bcc6
BoxExchanger <139485090+boxexchanger@users.noreply.github.com>
2024-10-09 17:38:41 +0400

    Update entry.sh
    fix lifecycle manager script

--- tron-lifecycle-manager/entry.sh
+++ tron-lifecycle-manager/entry.sh
@@ -2,7 +2,7 @@
 
 echo "Starting Tron Lifecycle Manager"
 
-printenv | grep -v "MONGO" >> /etc/environment
+printenv | grep "MONGO" >> /etc/environment
 
 echo "6 * * * * /usr/local/bin/python /app/cleanup.py > /proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/crontab
 
